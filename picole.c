f17c0b7878b4156d8732cb151d03d1c71bfa447d
DanielDiniz <daniel.brito.708@ufrn.edu.br>
2025-03-24 20:13:18 -0300

    Segunda tela ( tela_sobre)

--- picole.c
+++ picole.c
@@ -12,10 +12,12 @@
 
 //Assinatura das funções
 void tela_principal(void);
+void tela_sobre(void);
 
 
 int main (void) {
     tela_principal();
+    tela_sobre();
     return 0;
 } 
 
@@ -28,11 +30,11 @@ void tela_principal(void) {
     printf("******************************************************************\n");
     printf("***                                                            ***\n");
     printf("***     Universidade Federal do Rio Grande do Norte            ***\n");
-    printf("***          Centro de Ensino Superior do Seridó               ***\n");
-    printf("***        Departamento de Computação e Tecnologia             ***\n");
-    printf("***          Disciplina DCT1106 -- Programação                 ***\n");
-    printf("***         Projeto Sistema de Controle de Produção            ***\n");
-    printf("***       Developed by ? -- since Mar, 2025          ***\n");
+    printf("***          Centro de Ensino Superior do Serido               ***\n");
+    printf("***        Departamento de Computaçao e Tecnologia             ***\n");
+    printf("***          Disciplina DCT1106 -- Programaçao                 ***\n");
+    printf("***         Projeto Sistema de Controle de Producao            ***\n");
+    printf("***       Developed by Daniel Diniz -- since Mar, 2025         ***\n");
     printf("***                                                            ***\n");
     printf("******************************************************************\n");
     printf("***                                                            ***\n");
@@ -47,3 +49,28 @@ void tela_principal(void) {
     printf("******************************************************************\n");
     printf("\n");
 } 
+
+void tela_sobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Controle de producao                ///\n");
+    printf("///             Developed by Daniel Diniz -- since Mar, 2025                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Controle de producao = = = = =          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa dedicado a disciplina DCT1106 - Programação, para avaliacao e ///\n");
+    printf("///  desenpenho dos alunos mediado pelo Prof. Dr. Flavius Gorgonio de modo  ///\n");
+    printf("///  a mostrar o conhecimento adiquirido ao longo da disciplina.            ///\n");
+    printf("///  Projeto Sistema de controle de producao é um sistema desenvolvido para ///\n");
+    printf("///  auxiliar gerenciamento de producao de uma fabrica de picole artesanal  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}

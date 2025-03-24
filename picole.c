f4287d7f2051ad224c0450824f742599813c0635
Daniel Diniz <87156807+danielrito777@users.noreply.github.com>
2025-03-24 17:57:13 -0300

    Update picole.c

--- picole.c
+++ picole.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 int main (void) {
-    printf ("bem vindo ao projeto da fabriica de picoles\n");
+    printf ("bem vindo ao projeto da fabrica de picoles\n");
+
+
+**************************************************************
+***     Universidade Federal do Rio Grande do Norte        ***
+***        Centro de Ensino Superior do Seridó             ***
+***       Departamento de Computação e Tecnologia          ***
+***       Projeto Sistema de Controle de Produção          ***
+***       Developed by ? -- since Mar,2025       ***
+**************************************************************
+***                      Semana 1                          ***
+**************************************************************
+
+#include <stdio.h>
+
+******
+*** Assinatura das funções
+void tela_principal(void);
+void tela_sobre(void);
+void tela_equipe(void);
     return 0;
-}
+} 
+
+
+******
+** Funções
+
+void tela_principal(void) {
+    printf("\n");
+    printf("******************************************************************\n");
+    ptintf("***                                                            ***\n");
+    printf("***     Universidade Federal do Rio Grande do Norte            ***\n");
+    printf("***          Centro de Ensino Superior do Seridó               ***\n");
+    printf("***        Departamento de Computação e Tecnologia             ***\n");
+    printf("***          Disciplina DCT1106 -- Programação                 ***\n");
+    printf("***         Projeto Sistema de Controle de Produção            ***\n");
+    printf("***       Developed by ? -- since Mar, 2025          ***\n");
+    printf("***                                                            ***\n");
+    printf("******************************************************************\n");
+    printf("***                                                            ***\n");
+    printf("***    = = = = = Sistema de Controle de Produção = = = = =     ***\n");
+    printf("***                                                            ***\n");
+    printf("***            1. Módulo Manter insumos                        ***\n");
+    printf("***            2. Módulo manter receitas                       ***\n");
+    printf("***            3. Módulo manter produção                       ***\n");
+    printf("***            4. Módulo Manter vendas                         ***\n");
+    printf("***            0. Sair                                         ***\n");
+    printf("***                                                            ***\n");
+    printf("******************************************************************\n");
+    printf("\n");
+} 

04b41043a47f0890072c56561f180636ee1e14a4
DanielDiniz <daniel.brito.708@ufrn.edu.br>
2025-03-24 18:16:44 -0300

    Tela principal

--- picole.c
+++ picole.c
@@ -1,36 +1,32 @@
-#include <stdio.h>
-int main (void) {
-    printf ("bem vindo ao projeto da fabrica de picoles\n");
-
-
-**************************************************************
-***     Universidade Federal do Rio Grande do Norte        ***
-***        Centro de Ensino Superior do Seridó             ***
-***       Departamento de Computação e Tecnologia          ***
-***       Projeto Sistema de Controle de Produção          ***
-***       Developed by ? -- since Mar,2025       ***
-**************************************************************
-***                      Semana 1                          ***
-**************************************************************
+//***************************************************************
+// ***     Universidade Federal do Rio Grande do Norte        ***
+// ***        Centro de Ensino Superior do Seridó             ***
+// ***       Departamento de Computação e Tecnologia          ***
+// ***       Projeto Sistema de Controle de Produção          ***
+// ***       Developed by Daniel Diniz -- since Mar,2025      ***
+// **************************************************************
+// ***                      Semana 1                          ***
+// **************************************************************
 
 #include <stdio.h>
 
-******
-*** Assinatura das funções
+//Assinatura das funções
 void tela_principal(void);
-void tela_sobre(void);
-void tela_equipe(void);
+
+
+int main (void) {
+    tela_principal();
     return 0;
 } 
 
 
-******
-** Funções
+////
+// Funções
 
 void tela_principal(void) {
     printf("\n");
     printf("******************************************************************\n");
-    ptintf("***                                                            ***\n");
+    printf("***                                                            ***\n");
     printf("***     Universidade Federal do Rio Grande do Norte            ***\n");
     printf("***          Centro de Ensino Superior do Seridó               ***\n");
     printf("***        Departamento de Computação e Tecnologia             ***\n");

094332ebbf059278fe13421bdf8702426d41a84d
DanielDiniz <daniel.brito.708@ufrn.edu.br>
2025-03-24 21:01:05 -0300

    terceira tela ( tela equipe)

--- picole.c
+++ picole.c
@@ -1,8 +1,8 @@
 //***************************************************************
 // ***     Universidade Federal do Rio Grande do Norte        ***
-// ***        Centro de Ensino Superior do Seridó             ***
-// ***       Departamento de Computação e Tecnologia          ***
-// ***       Projeto Sistema de Controle de Produção          ***
+// ***        Centro de Ensino Superior do Serido             ***
+// ***       Departamento de Computacao e Tecnologia          ***
+// ***       Projeto Sistema de Controle de Producao          ***
 // ***       Developed by Daniel Diniz -- since Mar,2025      ***
 // **************************************************************
 // ***                      Semana 1                          ***
@@ -13,11 +13,13 @@
 //Assinatura das funções
 void tela_principal(void);
 void tela_sobre(void);
+void tela_equipe(void);
 
 
 int main (void) {
     tela_principal();
     tela_sobre();
+    tela_equipe();
     return 0;
 } 
 
@@ -74,3 +76,29 @@ void tela_sobre(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
+
+void tela_equipe(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Serido                     ///\n");
+    printf("///               Departamento de Computacao e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programacao                      ///\n");
+    printf("///                  Projeto Sistema de Controle de producao                ///\n");
+    printf("///             Developed by Daniel Diniz -- since Mar, 2025                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Controle de Producao = = = = =          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Este projeto exemplo foi desenvolvido por:                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Daniel Diniz Brito                                           ///\n");
+    printf("///            E-mail: daniel.brito.708@ufrn.br                             ///\n");
+    printf("///            Redes sociais: @danielbri_to                                 ///\n");
+    printf("///            Git: https://github.com/danielrito777/projeto.fabrica.picole ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+} 
